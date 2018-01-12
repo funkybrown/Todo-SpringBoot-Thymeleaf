f4b2c2f7f23724b6ad94dfd0f9c64d6a8a7a058e
ben brown <ben@debianben.funkybrown.co.uk>
2018-01-12 20:07:50 +0000

    extra post test

--- src/test/java/com/funkybrown/Todo/MockMvcWebTests.java
+++ src/test/java/com/funkybrown/Todo/MockMvcWebTests.java
@@ -5,16 +5,24 @@
  */
 package com.funkybrown.Todo;
 
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -40,19 +48,45 @@ public class MockMvcWebTests {
     @Test
     public void homePage() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders.get("/"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("content"));
     }
     
     @Test
     public void TasksPage() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders.get("/tasks"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("tasks"));
+                //.andExpect(MockMvcResultMatchers.model().attribute("tasks", Matchers.is(Matchers.empty())));
     }
     
     @Test
     public void CompletedTasksPage() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders.get("/completed"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("index"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("tasks"));
+                //.andExpect(MockMvcResultMatchers.model().attribute("tasks", Matchers.is(Matchers.empty())));
+    }
+    
+    @Test
+    public void PostTask() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.post("/tasks")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("name", "Test Task12345"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/tasks"));
+        
+        Task expectedTask = new Task();
+        expectedTask.setName("Test Task12345");
+        
+        mockMvc.perform(MockMvcRequestBuilders.get("/task/Test Task12345"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("index"))
+            .andExpect(model().attributeExists("tasks"))
+            .andExpect(model().attribute("tasks", hasSize(greaterThan(0))));
     }
     
 }
